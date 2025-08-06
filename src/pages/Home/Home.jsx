31d4d5c2efa0ad2faea17c75b410ca290ed84672
aramirez-neural <aramirez@neural.es>
2025-08-06 13:27:20 +0200

    así está bien.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -26,8 +26,39 @@ export default function Home() {
 
   return (
     <main ref={homeRef} className="main-container home-container">
-      <h1>Página de Inicio</h1>
-      <p>Este es el contenido principal del sitio.</p>
+      <div className="home-content-wrapper">
+        <div className="home-left-column">
+          <h1>neural</h1>
+          <div className="home-text-container">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum,
+              voluptates odit? Aspernatur nam dignissimos quo beatae veritatis
+              hic delectus incidunt molestias error amet pariatur, sit
+              necessitatibus illo, alias earum itaque? Lorem ipsum dolor sit
+              amet consectetur adipisicing elit. Repudiandae veritatis optio
+              molestiae possimus distinctio facere voluptate delectus libero
+              cupiditate, enim dolorem a incidunt deleniti quam, beatae tempore
+              architecto doloremque accusamus.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum,
+              voluptates odit? Aspernatur nam dignissimos quo beatae veritatis
+              hic delectus incidunt molestias error amet pariatur, sit
+              necessitatibus illo, alias earum itaque? Lorem ipsum dolor sit
+              amet consectetur adipisicing elit. Repudiandae veritatis optio
+              molestiae possimus distinctio facere voluptate delectus libero
+              cupiditate, enim dolorem a incidunt deleniti quam, beatae tempore
+              architecto doloremque accusamus.
+            </p>
+          </div>
+        </div>
+
+        <div className="home-right-column">
+          <p className="home-vertical-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
